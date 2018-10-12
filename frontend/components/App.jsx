abbec42f360f72d598cf93eb740f4d74b35c8058
matthew voorhees <voorhees.matthew@gmail.com>
2018-10-11 17:15:24 -0700

    Rename bars folder

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom'
 
-import TopBarContainer from './top_bar/top_bar_container';
+import TopBarContainer from './bars/top_bar_container';
 import LoginFormContainer from './session_form/login_form_container';
 import SignupFormContainer from './session_form/signup_form_container';
 import SplashPage from './splash_page/splash_page';
