988625b2da648a80d15e9f08b6748c8170ebf642
matthew voorhees <voorhees.matthew@gmail.com>
2018-10-13 11:21:17 -0700

    Restructure NavBar to class

--- frontend/components/main/bars/nav_bar.jsx
+++ frontend/components/main/bars/nav_bar.jsx
@@ -1,45 +1,48 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
+class NavBar extends React.Component {
+  constructor(props) {
+    super(props)
+  }
 
-
-const NavBar = ({ currentUser }) => {
-
-
-  const inMatsanaNavBar = () => (
-    <div className="nav-outer">
-      <div className="nav-header top-left">
+  render() {
+    return (
+      <div className="nav-outer">
+        <div className="nav-header top-left">
           matsana
-      </div>
-
-      <div className="nav-bar-contents">
-        <div className="nav-sections">
-          <div>Home</div>
-          <div>Tasks</div>
         </div>
-        <div className="nav-projects">
-          <ul>projects
-            <li>Fullstack</li>
-            <li>Another Project</li>
-            <li>Another Project</li>
-            <li>Another Project</li>
-            <li>Another Project</li>
-            <li>Another Project</li>
-            <li>Another Project</li>
-            <li>Another Project</li>
-            <li>Another Project</li>
-          </ul>
+
+        <div className="nav-bar-contents">
+          <div className="nav-sections">
+            <div>Home</div>
+            <div>Tasks</div>
+          </div>
+          <div className="nav-projects">
+            <ul>projects
+              <li>Fullstack</li>
+              <li>Another Project</li>
+              <li>Another Project</li>
+              <li>Another Project</li>
+              <li>Another Project</li>
+              <li>Another Project</li>
+              <li>Another Project</li>
+              <li>Another Project</li>
+              <li>Another Project</li>
+            </ul>
+          </div>
+        </div>
+        <div className="nav-footer">
+          LinkedIn Github Etc
         </div>
       </div>
-      <div className="nav-footer">
-        LinkedIn Github Etc
-      </div>
-    </div>
-  );
-
-  // return currentUser ? inMatsanaBar() : welcomeBar();
-  return currentUser ? inMatsanaNavBar() : null;
-};
+    );
+  }
 
 
-export default NavBar;
+
+
+}
+
+export default withRouter(NavBar);
