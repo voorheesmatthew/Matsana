f133cbd4fb13beb809fd00949e38be16ecac72b9
matthew voorhees <voorhees.matthew@gmail.com>
2018-10-14 12:38:48 -0700

    Fix nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
+
     resources :users, only: [:create] do
-      resources :teams, only: [:create, :update, :index, :show, :destroy]
+      resources :teams, only: [:index]
     end
+    resources :teams, only: [:create, :update, :show, :destroy]
 
     resources :teams, only: [] do
-      resources :projects, only: [:create, :update, :index, :show, :destroy]
+      resources :projects, only: [:index]
     end
+    resources :projects, only: [:create, :update, :show, :destroy]
     resource :session, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
